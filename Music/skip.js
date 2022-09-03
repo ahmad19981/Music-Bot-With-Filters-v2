3281a1a0e17c98c90cbfc3bdc5b9f605b140f011
ahmad19981 <53021224+ahmad19981@users.noreply.github.com>
2022-09-03 11:37:36 +0300

    Update skip.js

--- Music/skip.js
+++ Music/skip.js
@@ -1,5 +1,5 @@
 ////////////////////////////
-////////CONFIG LOAD/////////
+//////CONFIG LOAD///////////
 ////////////////////////////
 const { canModifyQueue } = require("../util/nkm");
 const { Client, Collection, MessageEmbed } = require("discord.js");
@@ -9,51 +9,31 @@ const { approveemoji, denyemoji, PREFIX, } = require(`../config.json`);
 //////COMMAND BEGIN/////////
 ////////////////////////////
 module.exports = {
-  name: "skipto",
-  aliases: ["st", "jump"],
-  description: "Skip to the selected queue number",
+  name: "skip",
+  aliases: ["se"],
+  description: "Skip the currently playing song",
   cooldown: 5,
-  edesc: `Type the Command, to skip a specific amount of songs to the wanted song.\nUsage: ${PREFIX}skipto`,
+  edesc: `Type the Command, to skip to current listening song.\nUsage: ${PREFIX}skip`,
 
-execute(message, args) {
-    //if not in a guild return
+execute(message) {
+    //if not in a guild retunr
     if (!message.guild) return;
-    //react with approve
+    //react with approve emoji
     message.react(approveemoji).catch(console.error);
-    //if no args return error
-    if (!args.length)
-      return attentionembed(message, `Try: ${message.client.prefix}${module.exports.name} <Queue Number>`)
-    //if not a number return error
-    if (isNaN(args[0]))
-      return attentionembed(message, `Try: ${message.client.prefix}${module.exports.name} <Queue Number>`)
     //get the queue
     const queue = message.client.queue.get(message.guild.id);
     //if no Queue return error
-    if (!queue) return attentionembed(message, "There is no Queue");
-    //if member not in the same voice channel as the Bot return
+    if (!queue)
+      return attentionembed(message, "There is nothing you can skip!").catch(console.error);
+    //if not in the same channel return
     if (!canModifyQueue(message.member)) return;
-    //if args bigger then the Server Queue return error
-    if (args[0] > queue.songs.length)
-      return attentionembed(message, `The Queue is just ${queue.songs.length} long!`);
-    //set playing to true
+    //set playing to true 
     queue.playing = true;
-    //if the queue is loop 
-    if (queue.loop) {
-      //make a loop for all songs to skip and add them at the end again
-      for (let i = 0; i < args[0] - 1; i++) 
-        queue.songs.push(queue.songs.shift());
-    //if not a loop
-    } else {
-      //remove all songs including the args 
-      queue.songs = queue.songs.slice(args[0] - 1);
-    }
     //end current song
     queue.connection.dispatcher.end();
-    //Send approve
+    //send approve message
     queue.textChannel.send(
-      new MessageEmbed()
-        .setColor("#F0EAD6")
-        .setAuthor(`${message.author.username}#${message.author.discriminator} skipped ${args[0]} songs`, "https://cdn.discordapp.com/attachments/778600026280558617/781024479623118878/ezgif.com-gif-maker_1.gif")
+      new MessageEmbed().setColor("#F0EAD6").setAuthor(`${message.author.username} skipped the song.`, "https://cdn.discordapp.com/attachments/778600026280558617/781024479623118878/ezgif.com-gif-maker_1.gif")
     ).catch(console.error);
   }
 };
